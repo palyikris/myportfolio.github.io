1c41d0a6cf1c2a93c80b8d9c406e7d03d78b3666
kris <palyi.kristof@gmail.com>
2021-12-05 21:45:25 +0100

    upload nextjs files

---
+++ nextjscrashcourse/pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404(){
+    return <h1>kurva anyád, ilyen oldal nincs</h1>
+}
---
+++ nextjscrashcourse/pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Layout from "../../components/layout"
+import { getAllPostsId, getPostData } from "../../lib/posts"
+import Head from "next/head"
+import Date from '../../components/date'
+import utilStyles from "../../styles/utils.module.css"
+
+export async function getStaticProps({params}){
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
+export async function getStaticPaths(){
+    const paths = getAllPostsId()
+    return {
+        paths,
+        fallback: false
+    }
+}
+export default function Post({postData}){
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+                <br />
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <br />
+                <div dangerouslySetInnerHTML = {{ __html: postData.contentHtml}} />
+            </article>
+        </Layout>
+    )
+}
