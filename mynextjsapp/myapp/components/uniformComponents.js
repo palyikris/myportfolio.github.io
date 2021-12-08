d35ee11b9885b1e8930477d428b88e50dd52ba13
kris <palyi.kristof@gmail.com>
2021-12-09 00:27:31 +0100

    new_features_added

--- mynextjsapp/myapp/components/uniformComponents.js
+++ mynextjsapp/myapp/components/uniformComponents.js
@@ -6,12 +6,14 @@ export default function myHead({children}){
     return (
         <div>
             <Head>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" /> 
             <meta charset="UTF-8" />
             <meta http-equiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Kristóf</title>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
             <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300&display=swap" rel="stylesheet" />
             </Head>
             <topnav className={styles.topnav}>
--- mynextjsapp/myapp/pages/index.js
+++ mynextjsapp/myapp/pages/index.js
@@ -15,33 +15,40 @@ export async function getStaticProps(){
 }
 
 export default function Home(allFeaturesData) {
+  function rollOut(){
+    const servicesAll = document.querySelectorAll(".service");
+    console.log(servicesAll)
+  }
+  
   return (
     <Uniform>
+      
       <Head>
         <title>Kristóf</title>
       </Head>
-      <div className={styles.container}>
+      <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+      <script>AOS.init();</script>
+      <div className={styles.container} data-aos-easing="linear"
+     data-aos-duration="1500">
         <div className={styles.introduction}>
-          <div className={styles.introText}>
-            <h1>Helló, én Kristóf vagyok!</h1>
-            <h4 className={styles.introductoryText}>Szeretnél magadnak egy szuper weboldalt?<br></br>
-              Egy olyat, ami interaktív, szép, és tud egy rakás<br></br> funkciót, amit csak szeretnél tényleg.<br></br><br></br>
+          <div className={styles.introText} >
+            <h1 data-aos="zoom-in">Helló, én <span className={styles.myName}>Kristóf</span> vagyok, <span className={styles.myName}>weblapfejlesztő.</span></h1>
+            <h4 className={styles.introductoryText} data-aos="zoom-in">Szeretnél magadnak egy szuper weboldalt?
+              Talán egy olyat, ami interaktív, szép, és tud egy rakás funkciót, amit csak szeretnél.<br></br><br></br>
               Ha igen, akkor remek helyre jöttél. <span className={styles.greetingSpan}>Üdvözöllek!</span>
             </h4>
           </div>
-          <div className={styles.clippy}>
-            <div className={styles.clippedDiv}>
-              <div className={styles.clippedDivInterior}></div>
-            </div>
-          </div>
+          <Image src="/../public/logo.png" width="500vmin" height="250vmin">
+
+          </Image>
         </div>
         <div className={styles.separator}></div>
         <div className={styles.featuresContainer}>
-        <h1 className={styles.featuresH1}>Válaszd ki mit szeretnél!</h1>
+        <h1 className={styles.featuresH1}>Válaszd ki mi érdekel!</h1>
           <div className={styles.featuresList}>
               {allFeaturesData.allFeaturesData.map(({id, title, description}) => (
                 <div className={styles.featureContainer}>
-                  <div className={styles.feature}>
+                  <div className={styles.feature} data-aos="zoom-in">
                   <div className={styles.featureTitle}>{id}</div>
                   <div className={styles.featureDesc}><p>{description}</p></div>
                   <Link href={`/${id}`}>
@@ -51,6 +58,59 @@ export default function Home(allFeaturesData) {
                 </div>
               ))}
           </div>
+        </div>
+        <div className={styles.separatorOther}></div>
+        <div className={styles.servicesContainer}>
+          <div className={styles.servicesTitle}><p>Amiket kínálok...</p></div>
+          <div className={styles.services}>
+            <div className={styles.service} data-aos="flip-left">
+              <div className={styles.serviceLogo}>
+              </div>
+              <div className={styles.serviceTitle}>
+                <p>Multiplatform fejlesztés</p>
+              </div>
+              <div className={styles.serviceDescription} >
+                <p>Webappok, weblapok, többféle programozási nyelven is, többféle eszközre, többféle céllal és működéssel. Természetesen minden reszponzív.</p>
+              </div>
+            </div>
+            <div className={styles.service} data-aos="flip-right">
+              <div className={styles.serviceLogo}></div>
+              <div className={styles.serviceTitle}><p>Rugalmasság</p></div>
+              <div className={styles.serviceDescription}>
+                <p>A kinézetet illetve a Te kezedben van a döntés. Minden úgy fog történni, ahogy elképzeled, de ha rám bízod, akkor se jársz rosszul. Gyorsan és rugalmasan kezelem a változtatásokat, egyéni kéréseket.</p>
+              </div>
+            </div>
+            <div className={styles.service} data-aos="flip-left">
+              <div className={styles.serviceLogo}></div>
+              <div className={styles.serviceTitle}><p>Szép, egységes tervezés</p></div>
+              <div className={styles.serviceDescription}>
+                <p>Mindenféle képpen célom, hogy amit Te megkapsz, a lehető legszebben, legletisztultabban nézzen ki, illetve, hogy tökéletesen töltse be a neki szánt szerepet.</p>
+              </div>
+            </div>
+            <div className={styles.service} data-aos="flip-right">
+              <div className={styles.serviceLogo}></div>
+              <div className={styles.serviceTitle}><p>Gyorsaság és jó ár</p></div>
+              <div className={styles.serviceDescription}>
+                <p>17 éves fejlesztő vagyok, ezért nyilván nem kérhetem el egy diplomás és sokkal tapasztaltabb programozó árát. Viszont minőségbeli különbséget garantálom, hogy nem fogsz tapasztalni, ellenben sokkal olcsóbban dolgozom.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.separator}></div>
+        <div className={styles.iceBreakerContainer}>
+                <div className={styles.iceBreaker}>
+                  <div className={styles.iceBreakerText1}>
+                    <p className={styles.iceBreakerTag}>&lt;Beszélgetéskezdeményező&gt;</p>
+                    <p>17 éves vagyok és a Karinthy Frigyes Gimnáziumba járok. Szeretek szinte minden sportot a focin kívül, és imádok beszélni.</p>
+                    <p className={styles.iceBreakerTag}>&lt;/Beszélgetéskezdeményező&gt;</p>
+                  </div>
+                  <div className={styles.iceBreakerSep}></div>
+                  <div className={styles.iceBreakerText2}>
+                    <p className={styles.iceBreakerTag}>&lt;Beszélgetéskezdeményező&gt;</p>
+                    <p>Szeretek rock zenét hallgatni és olvasni. A Twenty One Pilots zenéjét különösen szeretem. Sajnos nagyon szeretem az energiaitalt.</p>
+                    <p className={styles.iceBreakerTag}>&lt;/Beszélgetéskezdeményező&gt;</p>
+                  </div>
+                </div>
         </div>
       </div>
     </Uniform>
