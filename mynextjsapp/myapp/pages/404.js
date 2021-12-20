0ba9b00d64e08b570f4c7d550bb96ff6f6047f5b
kris <palyi.kristof@gmail.com>
2021-12-20 12:17:02 +0100

    examples_ready

---
+++ mynextjsapp/myapp/pages/404.js
@@ -0,0 +1,11 @@
+import Uniform from "../components/uniformComponents"
+
+export default function Error(){
+    return(
+    <Uniform>
+        <div>
+            <h1>Ilyen oldal nincs, te buta fasz.</h1>
+        </div>
+    </Uniform>
+    )
+}
---
+++ mynextjsapp/myapp/pages/examples.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import Uniform from "../components/uniformComponents"
+import Image from "next/image"
+import styles from "../styles/topnavs.module.css"
+
+export default function Example(){
+    return(
+    <Uniform>
+        <Head>
+            <title>Példák</title>
+        </Head>
+        <div className={styles.container}>
+            <div className={styles.titleContainer}>
+                <h1>Eddigi munkáim</h1>
+            </div>
+            <div className={styles.exampleContainer}>
+                <div className={styles.example}>
+                    <div className={styles.exampleTitle}>Üzenetküldő felület</div>
+                    <Image src="/../public/messages.png" width="500vmin" height="250vmin" alt="messages">
+
+                    </Image>
+                    <p>Tökéletesen működő üzenetküldő webapplikáció. Az <b>Üzimüzi</b> kiírja az üzenet küldésének idejét, illetve két üzenetküldés között
+                    
+                    eltelt idő alapján illeszti be őket. Default módon csak az utolsó 10 üzenetet mutatja, de tudja az összeset is.</p>
+                </div>
+                <div className={styles.example}>
+                <div className={styles.exampleTitle}>Iskolai jegyzetfüzet</div>
+                    <Image src="/../public/classes.png" width="500vmin" height="250vmin" alt="classes">
+
+                    </Image>
+                    <p>Iskolai jegyzetfüzet ami tantárgyakra bontva menti a jegyzeteket, illetve egy rakás built-in funkcióval rendelkezezik, például az órarend értelmezése
+                        és kiírása. Képes mutatni az előző a jelenlegi és a következő órádat.                   </p>
+                </div>
+                <div className={styles.example}>
+                <div className={styles.exampleTitle}>Login és regisztrációs rendszer</div>
+                    <Image src="/../public/login.png" width="500vmin" height="250vmin" alt="loginsystem">
+
+                    </Image>
+                    <p>
+                        Egy teljesen kiépített loginrendszer, ami automatikusan menti és alkalmazza az új regisztrálókat és különböző tulajdonságaik alapján hoz fel nekik oldalakat.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </Uniform>
+    )
+}
